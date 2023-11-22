55d1f1334af740a240097b9d469b4719dfedb8e4
DutchCodeCurious <114599908+DutchCodeCurious@users.noreply.github.com>
2023-11-22 14:34:39 +0100

    First commit

---
+++ src/datafunctions/Dataservice.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+const BASE_URL = "http://localhost:3000";
+
+export function getEvents() {
+  return axios.get(`${BASE_URL}/events`);
+}
+
+export function getEvent(id) {
+  return axios.get(`${BASE_URL}/events/${id}`);
+}
+
+export function getUsers() {
+  return axios.get(`${BASE_URL}/users`);
+}
+
+export function getUser(id) {
+  return axios.get(`${BASE_URL}/users/?id=${id}`);
+}
+
+export function getCategories() {
+  return axios.get(`${BASE_URL}/categories`);
+}
+
+export function getCategory(id) {
+  let cat = axios.get(`${BASE_URL}/categories/${id}`);
+  return console.log(cat);
+}
